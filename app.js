733aa67aaf097e29e3df60e1928cddbd44129531
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-10-20 19:19:26 -0300

    Add: Novos limites de upload

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(cors({
 }));
 
 // Middleware para análise do corpo da solicitação
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({
-    limit: '50mb',
+    limit: '500mb',
     parameterLimit: 100000,
     extended: true
 }));
