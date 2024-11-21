f14324712ddca1ed3c56049b8e86adc12c740325
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-11-21 18:34:14 -0300

    Add acess all

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const setupRoutes = require('./routes');
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
